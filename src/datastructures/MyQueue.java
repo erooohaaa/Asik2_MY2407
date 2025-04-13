7156bb3cc2faccdf2a468fd68b6c599a35d038d7
Yerkanat <erkanatm249@gmail.com>
2025-04-13 21:36:03 +0500

    4th commit

--- src/datastructures/MyQueue.java
+++ src/datastructures/MyQueue.java
@@ -1,4 +1,39 @@
 package datastructures;
 
-public class MyQueue {
+import collections.MyLinkedList;
+
+public class MyQueue<T> {
+    private MyLinkedList<T> list;
+
+    public MyQueue() {
+        list = new MyLinkedList<>();
+    }
+
+    public void enqueue(T item) {
+        list.addLast(item);
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        T item = list.getFirst();
+        list.removeFirst();
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return list.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
--- src/datastructures/MyStack.java
+++ src/datastructures/MyStack.java
@@ -1,4 +1,39 @@
 package datastructures;
 
-public class MyStack {
+import collections.MyArrayList;
+
+public class MyStack<T> {
+    private MyArrayList<T> list;
+
+    public MyStack() {
+        list = new MyArrayList<>();
+    }
+
+    public void push(T item) {
+        list.add(item);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        T item = list.get(list.size() - 1);
+        list.removeLast();
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        return list.getLast();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
