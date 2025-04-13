5ca1f5801177dfc228195a0f4fa637ebf69fc057
Yerkanat <erkanatm249@gmail.com>
2025-04-13 21:29:20 +0500

    the basic implementation of MyArrayList

--- src/collections/MyArrayList.java
+++ src/collections/MyArrayList.java
@@ -2,7 +2,172 @@ package collections;
 
 import java.util.Iterator;
 
+public class MyArrayList<T> implements MyList<T> {
+    private Object[] data;
+    private int size;
 
-public class MyArrayList {
+    public MyArrayList() {
+        data = new Object[10];
+        size = 0;
+    }
 
+    private void ensureCapacity() {
+        if (size >= data.length) {
+            Object[] newData = new Object[data.length * 2];
+            for (int i = 0; i < size; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        ensureCapacity();
+        data[size++] = item;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        checkIndex(index);
+        data[index] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new RuntimeException("Invalid index");
+        }
+        ensureCapacity();
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return (T) data[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        checkIndex(index);
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[--size] = null;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                Comparable<T> a = (Comparable<T>) data[j];
+                T b = (T) data[j + 1];
+                if (a.compareTo(b) > 0) {
+                    Object temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = data[i];
+        }
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        data = new Object[10];
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new RuntimeException("Index out of bounds");
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int current = 0;
+
+            public boolean hasNext() {
+                return current < size;
+            }
+
+            public T next() {
+                if (!hasNext()) {
+                    throw new RuntimeException("No more elements");
+                }
+                return (T) data[current++];
+            }
+        };
+    }
 }
