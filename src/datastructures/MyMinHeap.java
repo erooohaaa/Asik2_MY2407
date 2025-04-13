94d2ad35e63310f623706929fde9b6a2fd82cf64
Yerkanat <erkanatm249@gmail.com>
2025-04-13 21:37:13 +0500

    5th commit

--- src/datastructures/MyMinHeap.java
+++ src/datastructures/MyMinHeap.java
@@ -1,4 +1,72 @@
 package datastructures;
 
-public class MyMinHeap {
+import collections.MyArrayList;
+
+public class MyMinHeap<T extends Comparable<T>> {
+    private MyArrayList<T> heap;
+
+    public MyMinHeap() {
+        heap = new MyArrayList<>();
+    }
+
+    public void insert(T item) {
+        heap.add(item);
+        heapifyUp(heap.size() - 1);
+    }
+
+    public T extractMin() {
+        if (isEmpty()) {
+            throw new RuntimeException("Heap is empty");
+        }
+        T min = heap.get(0);
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.removeLast();
+        heapifyDown(0);
+        return min;
+    }
+
+    public T peekMin() {
+        if (isEmpty()) {
+            throw new RuntimeException("Heap is empty");
+        }
+        return heap.get(0);
+    }
+
+    public boolean isEmpty() {
+        return heap.size() == 0;
+    }
+
+    public int size() {
+        return heap.size();
+    }
+
+    private void heapifyUp(int index) {
+        while (index > 0 && heap.get(index).compareTo(heap.get((index - 1) / 2)) < 0) {
+            swap(index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    private void heapifyDown(int index) {
+        int leftChild = 2 * index + 1;
+        int rightChild = 2 * index + 2;
+        int smallest = index;
+
+        if (leftChild < heap.size() && heap.get(leftChild).compareTo(heap.get(smallest)) < 0) {
+            smallest = leftChild;
+        }
+        if (rightChild < heap.size() && heap.get(rightChild).compareTo(heap.get(smallest)) < 0) {
+            smallest = rightChild;
+        }
+        if (smallest != index) {
+            swap(index, smallest);
+            heapifyDown(smallest);
+        }
+    }
+
+    private void swap(int i, int j) {
+        T temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
 }
